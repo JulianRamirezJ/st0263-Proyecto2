ce7c47e21edb7255b6bed3a0bc219013bdd66472
Julian Giraldo Perez <jgpbak@outlook.com>
2023-05-15 20:53:19 -0500

    Added cpu utilization simulation to MonitorC

--- Monitor_C/monitor_c.py
+++ Monitor_C/monitor_c.py
@@ -1,23 +1,57 @@
 import grpc
 from concurrent import futures
+import threading
+import random
+
 
 from service_pb2_grpc import MonitorServicer, add_MonitorServicer_to_server
 from service_pb2 import Alive, Metric
 
+cpu_usage = random.choice([10,15,20])
+delay = 10
+current_delay = 0
+up = True
+
+def change_cpu():
+    global cpu_usage
+    global delay
+    global current_delay
+    global up
+    threading.Timer(2.0, change_cpu).start()
+    print(cpu_usage)
+    if current_delay > 0:
+        current_delay += 1
+        if(current_delay == delay):
+            current_delay = 0
+    else:
+        if up:
+            cpu_usage += 5
+        else:
+            cpu_usage -= 5
+        if cpu_usage == 95:
+            up = False
+            current_delay += 1
+        elif cpu_usage == 20:
+            up = True
+            current_delay += 1
+
+
 #Redefine methods or actions that we want the server to do when receibing an order
 class MonitorService(MonitorServicer):
+    global cpu_usage
     #request is the param we definedd in proto
 
     #This method test if server is alive
     def is_alive(self, request, context):
-        return Alive(alive = "Holi")
+        return Alive(alive = "True")
     
     #Register order takes order and response with OrderConfirmation
     def get_metric(self, request, context):
-        return Metric(delivery = 5)
+        return Metric(delivery = cpu_usage)
     
 
 def start():
+    change_cpu()
     #Inside this method we will start to generate our service
 
     #How many threads or concurrent services to execute at the same time
