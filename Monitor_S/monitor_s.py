92640b45a76705e19f8a03f1bb6be7cb5e82e0db
JulianRamirezJ <julianarj39000@gmail.com>
2023-05-16 15:57:24 -0500

    Port configuration and instance address

--- Monitor_S/monitor_s.py
+++ Monitor_S/monitor_s.py
@@ -8,17 +8,19 @@ from Monitor_S import service_pb2_grpc
 def start_process(instance_state, event):
     event.wait()
     print("M_S started")
+    PORT = ""
     while True:
-        process_instances(instance_state)
+        process_instances(instance_state,'50051')
         print(instance_state)
         t.sleep(3)
 
 
-def process_instances(instance_state):
+def process_instances(instance_state, PORT):
     for key in instance_state.keys():
         current_instance = instance_state[key]
+        instance_address = current_instance[1]+':'+PORT
         try:
-            channel = grpc.insecure_channel(current_instance[1])
+            channel = grpc.insecure_channel(instance_address)
             stub = service_pb2_grpc.MonitorStub(channel)
             alive = get_alive(stub)
             metric = 0
