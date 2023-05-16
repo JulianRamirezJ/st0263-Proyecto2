77b864cf7fd3d25c32ef514bc67d8633e9270da0
JulianRamirezJ <julianarj39000@gmail.com>
2023-05-15 20:19:43 -0500

    Monitor S communication with C

--- Controller/controller.py
+++ Controller/controller.py
@@ -2,5 +2,10 @@ import multiprocessing as mp
 import time
 
 def start_process(instance_state, event):
+    instance_state['instance01'] = [True,'localhost:50051',30]
+    instance_state['instance02'] = [True,'localhost:50052',30]
+    instance_state['instance03'] = [True,'localhost:50053',30]
+    instance_state['instance02'] = [True,'localhost:50054',30]
+    instance_state['instance02'] = [True,'localhost:50055',30]
     time.sleep(5)
     event.set()
